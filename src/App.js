bc8bfdb9e1bef2285ed3aef8b04eff10b11a74fa
Arup Mondal <mondalconnect17@gmail.com>
2021-12-10 11:05:02 +0530

    src files

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './components/About';
+import LoadingBar from 'react-top-loading-bar'
+
+
+export default class App extends Component {
+  pageSize = 6;
+  apiKey = process.env.REACT_APP_NEWS_API
+  //LOADING BAR ADDITIONAL METHOD
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <Router>
+            <NavBar />
+            <LoadingBar
+              color='green'
+              progress={this.state.progress}
+              height={3}
+            />
+            <Switch>
+              <Route exact path="/"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general" /> </Route>
+              <Route exact path="/general"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="generalx" pageSize={this.pageSize} country="in" category="general" /> </Route>
+              <Route exact path="/business"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business" /> </Route>
+              <Route exact path="/entertainment"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" /> </Route>
+              <Route exact path="/health"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health" /> </Route>
+              <Route exact path="/science"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science" /> </Route>
+              <Route exact path="/sports"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports" /> </Route>
+              <Route exact path="/technology"> <News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology" /> </Route>
+              {/* <Route exact path="/about"> <About />  </Route> */}
+            </Switch>
+          </Router>
+        </div>
+        <div className="foot">
+          <footer className=" footDesign"><a href="https://github.com/mondalCodeHub">&copy; mondalCodeHub </a></footer>
+        </div>
+      </>
+    )
+  }
+}
+
+
